dcca185248933f42660e267df01c5e92a11489da
Hardik12244 <hardik12244@gmail.com>
2025-11-20 19:38:41 +0530

    made env to protect mongo passowrd

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const routes = require("./routes/url");
 const {connectToMongo} = require("./connect");
+require ('dotenv').config();
 
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
-connectToMongo("mongodb+srv://hardik12244_db_user:SsTnGj2XdSe1qafV@cluster0.mi1pa1z.mongodb.net/")
+connectToMongo(process.env.MONGO_URL)
     .then(()=>{console.log("db connected")})
 
 app.use('/url',routes); 
