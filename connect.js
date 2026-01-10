95335db4a150db97b72890748482a5b28e7886e4
Hardik12244 <hardik12244@gmail.com>
2026-01-11 01:50:50 +0530

    mongo changes 2

--- connect.js
+++ connect.js
@@ -3,16 +3,13 @@ const mongoose = require("mongoose");
 let isConnected = false;
 
 async function connectToMongo(url) {
-  if (isConnected) {
-    return;
-  }
+  if (isConnected) return;
 
-  try {
-    await mongoose.connect(url);
-    isConnected = true;
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("Mongo connection error:", err);
-    throw err;
-  }
+  await mongoose.connect(url);
+  isConnected = true;
+  console.log("Mongo connected");
 }
+
+module.exports = {
+  connectToMongo,
+};
